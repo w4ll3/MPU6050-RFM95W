2744b6e6d4114e383aadd5b61735e9059d8304d5
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2018-08-09 18:27:28 -0300

    Add files via upload

---
+++ LoRaReceiver.ino
@@ -0,0 +1,34 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+// Pin definetion of WIFI LoRa 32
+#define SCK     5    // GPIO5  -- SX1278's SCK
+#define MISO    19   // GPIO19 -- SX1278's MISO
+#define MOSI    27   // GPIO27 -- SX1278's MOSI
+#define SS      18   // GPIO18 -- SX1278's CS
+#define RST     14   // GPIO14 -- SX1278's RESET
+#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
+
+#define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
+#define PABOOST true
+void setup() {
+  Serial.begin(115200);
+  while (!Serial); //test this program,you must connect this board to a computer
+  SPI.begin(SCK,MISO,MOSI,SS);
+  LoRa.setPins(SS,RST,DI0);
+  if (!LoRa.begin(BAND,PABOOST )) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    // read packet
+    while (LoRa.available())
+      Serial.print((char)LoRa.read());
+  }
+}
